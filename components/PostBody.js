46578aa2d7997c385ac4f11cf831e0c509e4404b
Gerald Nash <me@aunyks.com>
2020-06-09 18:22:15 -0400

    Better spacing on large screens

--- components/PostBody.js
+++ components/PostBody.js
@@ -1,6 +1,6 @@
 export default function PostBody({ children }) {
   return (
-    <section className="px-3 lg:px-48 pt-12 lg:pt-16">
+    <section className="mx-auto px-3 lg:px-0 w-full lg:w-1/2 pt-12 lg:pt-16">
       {children}
     </section>
   )
