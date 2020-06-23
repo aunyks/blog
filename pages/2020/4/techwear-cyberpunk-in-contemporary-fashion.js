3d8a56781bb3dc9b77c75ffdf3cef1b1093b84cf
Gerald Nash <me@aunyks.com>
2020-06-22 23:31:01 -0400

    post fix

--- pages/2020/4/techwear-cyberpunk-in-contemporary-fashion.js
+++ pages/2020/4/techwear-cyberpunk-in-contemporary-fashion.js
@@ -8,10 +8,11 @@ export default () => (
     description="The techwear and cyberpunk aesthetics are converging on a shared visual identity. How closely do this streetwear trend and sci-fi genre relate?"
     cardImage="https://blog.aunyks.com/img/tech-culture/cyberpunk-techwear-cover.png">
     <p>
-      Techwear is a trend in modern streetwear characterized by high-tech materials, high function, and urban influence. Components of techwear outfits are identified by high tech materials chosen to maximize the comfort of the wearer in response to the conditions of the environment. Freedom of motion is a high priority. Cold and wet environments inspire waterproof outer layers with insulating inner layers. Warmer climates inspire light and porous outer layers with moisture-wicking inner layers.
-      </p>
+      Techwear is a trend in modern streetwear characterized by modern materials, high function, and hints of urban influence. Components of techwear outfits are identified by modern materials chosen to maximize the comfort of the wearer in response to the conditions of the environment. Cold and wet environments inspire waterproof outer layers with insulating inner layers. Warmer climates inspire light and porous outer layers with moisture-wicking inner layers.
+      Techwear outfits also maintain great ranges of motion, which is reminiscent of athletic wear.
+    </p>
     <p>
-      The most interesting aspect of techwear is the boldness of its aesthetic. Neutral colors with sleek, futuristic silhouettes quickly remind us of the cyberpunk aesthetic, which is defined as "high tech, low life". This saying refers not only to the lifestyle but also the fashion. Fashion in the cyberpunk genre is often identified by in-your-face neon colors, cybernetic augmentations, masks, and other futuristic accessories and modifications.
+      The most interesting aspect of techwear is the boldness of its aesthetic. Neutral colors with sleek, futuristic silhouettes quickly remind us of the cyberpunk aesthetic, which is defined as "high tech, low life". This saying refers not only to cyberpunk lifestyle but also cyberpunk fashion. Fashion in the cyberpunk genre is often identified by in-your-face neon colors, cybernetic augmentations, masks, and other futuristic accessories and modifications.
     </p>
     <figure className="my-5">
       <img className="w-full lg:w-1/2" src="/img/tech-culture/cyberpunk-game-kneeling.jpg"
@@ -19,16 +20,16 @@ export default () => (
       <figcaption className="w-full lg:w-1/2 text-xs text-center mx-auto">Notice the LED accents woven into the jacket.</figcaption>
     </figure>
     <p>
-      Visually, the two aesthetics share some themes: high tech, ubiquitous neutrals, and high body coverage. Cyberpunk's high tech aspects include <a href="https://interestingengineering.com/top-9-cybernetic-upgrades-everyone-will-want">cybernetic augmentations</a> and clothing with sensors that react with the environment.
+      Visually, the two aesthetics share some themes: futuristic technology, ubiquitous neutrals, and high body coverage. Cyberpunk's high tech aspects include <a href="https://interestingengineering.com/top-9-cybernetic-upgrades-everyone-will-want">cybernetic augmentations</a> and clothing with sensors that react with the environment.
       </p>
     <p>
-      Techwear's high tech aspects include high tech materials and fabrics that uphold properties like breathability, insulation, or waterproofness while remaining low profile. It also includes accessories that passively adapt their utility to the wearer's environment, like filtering masks that can protect the wearer from disease or pollution.
+      Techwear's high tech aspects include modern materials and fabrics that uphold properties like breathability, insulation, or waterproofness while remaining low profile. These materials are not always naturally occurring and are made available only by modern materials science practices. It also includes accessories that passively adapt their utility to the wearer's environment, like filtering masks that can protect the wearer from disease or pollution.
       </p>
     <p>
-      Cyberpunk and techwear both maintain a lot of neutral colors, particularly blacks and grays. Cyberpunk may juxtapose this with neon accents, but techwear holds true to low profile neutrals, with occasional dark colors or dull whites.
+      Cyberpunk and techwear both maintain a lot of neutral colors, particularly blacks and grays. Cyberpunk may juxtapose this with neon accents, but techwear holds true to low profile neutrals, with occasional dull whites or non-black darks.
       </p>
     <p>
-      Techwear and cyberpunk also happen to share high body coverage. Compared to most other styles, a techwear or cyberpunk outfit will show much less of the wearer's skin in the same climate. Instead of skin, these outfits would reveal a tightly fitting under layer on parts of the body that don't have multiple layers, since layering is essential to a techwear outfit in any weather.
+      Techwear and cyberpunk also happen to share high body coverage. Compared to most other styles, a techwear or cyberpunk outfit will show much less of the wearer's skin in the same climate. Instead of skin, these outfits would reveal a tightly fitting under layer on parts of the body that don't have multiple layers, since layering is essential to a techwear outfit.
       </p>
     <figure className="my-5">
       <img className="w-full" src="/img/tech-culture/techwear-cyberpunk-sbs.jpg"
@@ -36,7 +37,7 @@ export default () => (
       <figcaption className="w-full text-xs mx-auto">This side-by-side embodies <a href="https://www.merriam-webster.com/dictionary/praxis">praxis</a>, where the theory is the character's cyberpunk outfit and the practice is the person's techwear outfit.</figcaption>
     </figure>
     <p>
-      These two aesthetics compliment one another and feel as though they're the same idea rendered in different settings. This leads us to refer to cyberpunk as itself but techwear as its contemporary counterpart. They both employ fashion technology to achieve some goal and have now converged upon a shared visual identity. Similarly, they both in some way antagonize the technological authority of high powers. Techwear outfits sometimes feature masks, which are known to combat facial recognition surveillance. Cyberpunk outfits do the same and may even feature more anti-authority technology such as sensor jamming technology among many other things.
+      These two aesthetics compliment one another and feel as though they're the same idea rendered in different settings. This leads us to refer to cyberpunk as itself but techwear as its contemporary counterpart. They both employ fashion technology to achieve some goal and have now converged upon a shared visual identity. Philosophically, both also antagonize the technological authority of high powers. Techwear outfits sometimes feature masks, which are known to combat facial recognition surveillance. Cyberpunk outfits do the same and may even feature more anti-authority technology such as sensor jamming technology among many other things.
     </p>
     <p>
       As digital technology and modern materials science extend their reach into fashion and cultural expression, will we see techwear become cyberpunk as we know it today? Will this modern fashion trend continue to expand out of its niche and reach the masses to become the norm? I'm sure time will tell.
