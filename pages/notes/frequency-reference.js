b9bfcbdfd40a6fd5570159558455d2e5c6980969
Gerald Nash <me@aunyks.com>
2020-08-16 18:09:47 -0400

    .

--- pages/notes/frequency-reference.js
+++ pages/notes/frequency-reference.js
@@ -3,7 +3,7 @@ import NotesPost from 'components/NotesPost'
 const Station = ({ title, freq, children }) => {
   return (
     <>
-      <h3 className="text-2xl">{title}</h3>
+      <h3 className="text-2xl leading-none mb-2">{title}</h3>
       <h4 className="text-xl">{freq}</h4>
       {children}
     </>
@@ -22,7 +22,7 @@ export default () => (
         Citizens Band Radio is a radio service that used to be very popular. Transmissions often travel for tens of miles.
       </p>
       <p>
-        <a target="_blank" href="https://www.fcc.gov/wireless/bureau-divisions/mobility-division/citizens-band-radio-service-cbrs">Frequencies ("Data" tab)</a>
+        <a target="_blank" href="https://www.fcc.gov/wireless/bureau-divisions/mobility-division/citizens-band-radio-service-cbrs">More info</a>
       </p>
     </Station>
     <Station title="ISS SSTV Downlink" freq="145.800 MHz">
@@ -36,7 +36,7 @@ export default () => (
         Some consumer walkie-talkies operate on these frequencies. Scanning up and down this range should have you land on some walkie-talkies operating on a couple of channels. Transmitting on these frequencies typically doesn't require a license.
       </p>
       <p>
-        <a target="_blank" href="https://www.fcc.gov/wireless/bureau-divisions/mobility-division/multi-use-radio-service-murs">Frequencies ("Data" tab)</a>
+        <a target="_blank" href="https://www.fcc.gov/wireless/bureau-divisions/mobility-division/multi-use-radio-service-murs">More info</a>
       </p>
     </Station>
     <Station title="NOAA Weather Broadcast" freq="162.500 MHz">
@@ -49,7 +49,7 @@ export default () => (
         Some consumer walkie-talkies operate on these frequencies, but transmitting on these require an FCC license in the US.
       </p>
       <p>
-        <a target="_blank" href="https://www.fcc.gov/general-mobile-radio-service-gmrs">Frequencies ("Data" tab)</a>
+        <a target="_blank" href="https://www.fcc.gov/general-mobile-radio-service-gmrs">More info</a>
       </p>
     </Station>
     <Station title="Family Radio Service (FRS)" freq="462 MHz - 467 MHz">
@@ -57,7 +57,7 @@ export default () => (
         Most consumer walkie-talkies operate on these frequencies. Scanning up and down this range should have you land on some walkie-talkies operating on a couple of channels. Transmitting on these frequencies typically doesn't require a license.
       </p>
       <p>
-        <a target="_blank" href="https://www.fcc.gov/wireless/bureau-divisions/mobility-division/family-radio-service-frs">Frequencies ("Data" tab)</a>
+        <a target="_blank" href="https://www.fcc.gov/wireless/bureau-divisions/mobility-division/family-radio-service-frs">More info</a>
       </p>
     </Station>
     <h2>AM</h2>
