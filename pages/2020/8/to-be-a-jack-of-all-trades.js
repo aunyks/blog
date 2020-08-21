5d45740af356875657c0f664389f88f946609f75
Gerald Nash <me@aunyks.com>
2020-08-20 21:32:24 -0400

    responsive quotes

--- pages/2020/8/to-be-a-jack-of-all-trades.js
+++ pages/2020/8/to-be-a-jack-of-all-trades.js
@@ -2,8 +2,8 @@ import Post from 'components/Post'
 
 const Quote = ({ children }) => {
   return (
-    <blockquote className="my-5 inline-block">
-      <span className="text-4xl font-serif bold leading-tight inline" style={{ color: 'white', background: '#5d44f8' }}>
+    <blockquote className="my-2 lg:my-5 inline-block">
+      <span className="text-2xl lg:text-4xl font-serif bold leading-tight inline" style={{ color: 'white', background: '#5d44f8' }}>
         "{children}"
       </span>
     </blockquote>
