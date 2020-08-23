bc5064987cd472fd9aa706c48dfe38938c9100ae
Gerald Nash <me@aunyks.com>
2020-08-23 15:02:46 -0400

    .

--- pages/2020/8/to-be-american-is-to-rebel.js
+++ pages/2020/8/to-be-american-is-to-rebel.js
@@ -15,7 +15,7 @@ export default () => (
     )}>
     <p>
       As American children grow up, we're indoctrinated in the American ideas of freedom and liberty. We're told that the Constitution, Bill of Rights, and always-growing list of amendments delivers them from any form of <Hint msg={`A more modern equivalent of the word "tyranny" is "oppression".`}>tyranny</Hint> that the government could impose.
-      Should we be dissatisfied with the status of their government, we know to wield our democratic power to cause the change they want to see.
+      Should we be dissatisfied with the status of our government, we know to wield our democratic power to cause the change they want to see.
       </p>
     <p>
       We learn that the founders of the country fought for these "inalienable" rights and that any time some government tries to infinge on them we must fight hard to reject it. We're told to look back at the founders and remind ourselves of the valor we should have in the face of tyranny.
@@ -29,15 +29,15 @@ export default () => (
       They suggest that law enforcement use "any means necessary" policies when trying to suppress protests. They want the government to infringe on these people's rights to prevent property damage and disruption to nearby individuals and businesses.
     </p>
     <p>
-      But, this is where the cognitive dissonance starts. If put in the conditions of the American revolution, these same patriots would be furious with the revolutionaries of the time. They'd call them the same names and want the British troops stationed in the colonies
+      If put in the conditions of the American revolution, these same patriots would be furious with the revolutionaries of the time. They'd call them the same names and want the British troops stationed in the colonies
       to quell any troublesome activity. As a matter of fact, those loyal to the crown did just that. Some loyalists went so far as to assist the British military in fighting the revolutionaries.
     </p>
     <p>
       Does that sound familiar? Supporting a government so much that you're willing to support its acts of tyranny against fellow citizens is something that not only loyalists did but also so-called "patriots" do. This is not to say that each time a group causes good trouble
-      they're trying to secede from the country and start anew. This is to say that causing trouble to see a better future is inherently American, by any means necessary like the founders of our country believed.
+      they're trying to secede from the country and start anew. This is to say that causing trouble to see a better future for more Americans <em>is</em> inherently American, by any means necessary like the founders of our country believed.
     </p>
     <p>
-      To be American is to fight for a better future. To be American is to rebel.
+      <em>To be American is to fight for a better future. To be American is to rebel.</em>
     </p>
   </Post>
 )
