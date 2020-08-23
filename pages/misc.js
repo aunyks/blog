891d965f2fbe8cb05ff2040564e6dd971df312a6
Gerald Nash <me@aunyks.com>
2020-08-23 14:02:24 -0400

    to be american

--- pages/misc.js
+++ pages/misc.js
@@ -6,6 +6,7 @@ export default () => (
     pageTitle="Misc"
     title="Misc"
     subtitle="Posts of no category">
+    <Link href="/2020/8/to-be-american-is-to-rebel">to be american is to rebel</Link>
     <Link href="/2020/8/designing-my-first-credit-card">designing my first credit card</Link>
     <Link href="/2020/8/to-be-a-jack-of-all-trades">to be a jack of all trades</Link>
   </LinksPage>
