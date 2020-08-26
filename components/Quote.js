dca9239b1fc6f27407e1ee4d263f773dd71d96f1
Gerald Nash <me@aunyks.com>
2020-08-25 23:36:10 -0400

    Writing "the cycle of human progress"

--- components/Quote.js
+++ components/Quote.js
@@ -1,9 +1,17 @@
-export default function Quote({ children }) {
+export default function Quote({ author, children }) {
   return (
     <blockquote className="my-2 lg:my-5 inline-block">
       <span className="text-2xl lg:text-4xl font-serif bold leading-tight inline" style={{ color: 'white', background: '#5d44f8' }}>
         "{children}"
       </span>
+      {!!author && (
+        <>
+          <br />
+          <span className="text-xl lg:text-3xl font-serif">
+            &mdash; {author}
+          </span>
+        </>
+      )}
     </blockquote>
   )
 }
---
+++ pages/2020/8/the-cycle-of-human-progress.js
@@ -0,0 +1,65 @@
+import Post from 'components/Post'
+import Quote from 'components/Quote'
+import Hint from 'components/Hint'
+
+export default () => (
+  <Post
+    title="The Cycle of Human Progress"
+    subtitle="Human Evolution in Three Dimensions"
+    description="Recounting the inspiration and process for designing my first credit card."
+    date="2020-08-26">
+    <p>
+      A couple weeks ago, I was watching an episode of <a href="https://www.netflix.com/title/80057883">Abstract</a> that covered Bio-Architecture. It followed <a href="https://twitter.com/NeriOxman">Neri Oxman</a> as she completed a project of hers, telling her life story in the process. She made lots of insightful statements, but one idea of hers that had truly stuck with me was how she described the connection between art, science, engineering,
+        and design.
+      </p>
+    <Quote author="Neri Oxman">
+      Science converts information into knowledge, engineering converts knowledge into utility, design converts utility into culture behavior and context, and art converts the culture behavior and question our perception of the world.
+      </Quote>
+    <p>
+      Here, she describes the Krebs Cycle of Creativity (KCC), into which she delves more deeply in her article titled <em><a href="https://jods.mitpress.mit.edu/pub/ageofentanglement/release/1">Age of Entanglement</a></em>. When first hearing her
+      describe such a cycle so succinctly, I was met with a new model of thinking of such concepts, but I was unsure what to make of it. "What do I do with this information," I questioned. She'd worded a relationship that's flew over the heads of millions, maybe billions of people in the past
+      so beautifully, but now that I understood the relationship I wasn't sure exactly what it meant in the grand scheme.
+    </p>
+    <p>
+      With enough time, I came to the conclusion that this idea describes the concept of human progress itself. Since the beginning of time, human creativity created art from the observation of nature,
+      artistic expression inspired scientific exploration, exploration inspired technological invention, and invention required design communication to provide true meaning to humans. It's at this point that new paradigms
+      have changed human culture and behavior, so the artists renew the cycle again.
+    </p>
+    <Quote author="Neri Oxman">
+      ...and when art meet science, it’s the Cinderella moment in the 12 o’clock.
+      </Quote>
+    <p>
+      Everything that the human race has created falls somewhere on this cycle and moves clockwise, inspiring more progress for others to take on. This is an interesting way to think of humanity, as many of us question
+      why any one of the quadrants exist. Unfortunately art gets the most criticism of this kind. People very often state that art is a waste of resources because of its economic significance isn't always great. This model
+      of thinking provides one more proof that art is equally impactful on human progress as the other three aspects, <Hint msg="Art has many purposes, and it shouldn't be measured solely by its economic impact. Much of science also has relatively little economic impact.">highlighting that artistic expression inspires other humans some means</Hint>.
+    </p>
+    <p>
+      A modern example of this is Bitcoin. It's said that Neal Stephenson's science fiction novel <em><a href="https://en.wikipedia.org/wiki/Cryptonomicon">Cryptonomicon</a></em> inspired the digital currency called Bitcoin. Here,
+      Stephenson produced the art via his novel. Several technologists like <a href="http://www.chaum.com/">David Chaum</a> and <a href="https://twitter.com/adam3us">Adam Back</a> have scientifically explored distributed digital currencies with Chaumian e-cash and Hashcash, respectively.
+      Their efforts likely inspired Satoshi Nakamoto to explore another digital currency implementation, and he later invented Bitcoin for the world to use. The following years, including now, have been dedicated to design thinkers trying to make Bitcoin
+      as accessible as possible to the average person. This, in turn has inspired more artistic cretion, and the cycle continues!
+    </p>
+    <p>
+      This cycle is often modeled visually with a circle reminding of us of a clock.
+    </p>
+    <figure className="my-2 mx-auto w-1/2">
+      <img alt="The Krebs Cycle of Creativity" src="/img/misc/kcc.png" />
+      <figcaption className="text-xs text-center">Source: <a href="https://jods.mitpress.mit.edu/pub/ageofentanglement/release/1">MIT JoDS</a></figcaption>
+    </figure>
+    <p>
+      This helps you visualize the clockwise motion in which creativity and inspiration travel. The image is in two dimensions, <Hint msg={`I don't believe in the "left brain"/"right brain" stuff, but I understand that a lot of people think of it this way.`}>with the "left brain"
+      activities on the right side and "right brain" activities on the left side</Hint> and the more abstract, theoretical activities on top and applied activities on the bottom. But, I think we can model it even more accurately if we think about it in three dimensions, with the third dimension abstractly
+      representing progress.
+    </p>
+    <figure className="my-2 mx-auto w-1/2">
+      <img alt="" src="/img/misc/kc.png" />
+      <figcaption className="text-xs text-center"></figcaption>
+    </figure>
+    <p>
+      With this, we can better visualize this idea in a grander scheme. We can see that although creativity and inspiration continuously cycle in the same two dimensions, the condition of humanity improves almost linearly.
+    </p>
+    <p>
+      I'll be keeping this in the back of my mind for a <em>long</em> time. Hopefully you will too.
+    </p>
+  </Post>
+)
