4130aaf2ebedde994c98155e7d4e192da91840ec
Gerald Nash <me@aunyks.com>
2020-08-26 00:38:31 -0400

    tweak cohp

--- pages/2020/8/the-cycle-of-human-progress.js
+++ pages/2020/8/the-cycle-of-human-progress.js
@@ -18,7 +18,7 @@ export default () => (
       </Quote>
     <p>
       Here, she describes the Krebs Cycle of Creativity (KCC), into which she delves more deeply in her article titled <em><a href="https://jods.mitpress.mit.edu/pub/ageofentanglement/release/1">Age of Entanglement</a></em>. When first hearing her
-      describe such a cycle so succinctly, I was met with a new model of thinking of such concepts, but I was unsure what to make of it. "What do I do with this information," I questioned. She'd worded a relationship that's flew over the heads of millions, maybe billions of people in the past
+      describe such a cycle so succinctly, I was met with a new model of thinking of such concepts, but I was unsure what to make of it. She'd worded a relationship that's flown over the heads of millions, maybe billions of people in the past
       so beautifully, but now that I understood the relationship I wasn't sure exactly what it meant in the grand scheme.
     </p>
     <p>
@@ -32,16 +32,16 @@ export default () => (
     <p>
       Everything that the human race has created falls somewhere on this cycle and moves clockwise, inspiring more progress for others to take on. This is an interesting way to think of humanity, as many of us question
       why any one of the quadrants exist. Unfortunately art gets the most criticism of this kind. People very often state that art is a waste of resources because of its economic significance isn't always great. This model
-      of thinking provides one more proof that art is equally impactful on human progress as the other three aspects, <Hint msg="Art has many purposes, and it shouldn't be measured solely by its economic impact. Much of science also has relatively little economic impact.">highlighting that artistic expression inspires other humans some means</Hint>.
+      of thinking provides one more proof that art is equally impactful on human progress as the other three aspects, <Hint msg="Art has many purposes, and it shouldn't be measured solely by its economic impact. Much of science also has relatively little economic impact.">highlighting that artistic expression inspires other humans by some means</Hint>.
     </p>
     <p>
-      A modern example of this is Bitcoin. It's said that Neal Stephenson's science fiction novel <em><a href="https://en.wikipedia.org/wiki/Cryptonomicon">Cryptonomicon</a></em> inspired the digital currency called Bitcoin. Here,
+      A modern example of the cycle is Bitcoin. It's said that Neal Stephenson's science fiction novel <em><a href="https://en.wikipedia.org/wiki/Cryptonomicon">Cryptonomicon</a></em> inspired the digital currency called Bitcoin. Here,
       Stephenson produced the art via his novel. Several technologists like <a href="http://www.chaum.com/">David Chaum</a> and <a href="https://twitter.com/adam3us">Adam Back</a> have scientifically explored distributed digital currencies with Chaumian e-cash and Hashcash, respectively.
       Their efforts likely inspired Satoshi Nakamoto to explore another digital currency implementation, and he later invented Bitcoin for the world to use. The following years, including now, have been dedicated to design thinkers trying to make Bitcoin
       as accessible as possible to the average person. This, in turn has inspired more artistic cretion, and the cycle continues!
     </p>
     <p>
-      This cycle is often modeled visually with a circle reminding of us of a clock.
+      The cycle is often modeled visually with a circle reminding of us of a clock.
     </p>
     <figure className="my-2 mx-auto w-1/2">
       <img alt="The Krebs Cycle of Creativity" src="/img/misc/kcc.png" />
@@ -50,10 +50,10 @@ export default () => (
     <p>
       This helps you visualize the clockwise motion in which creativity and inspiration travel. The image is in two dimensions, <Hint msg={`I don't believe in the "left brain"/"right brain" stuff, but I understand that a lot of people think of it this way.`}>with the "left brain"
       activities on the right side and "right brain" activities on the left side</Hint> and the more abstract, theoretical activities on top and applied activities on the bottom. But, I like to think that we can model it even more accurately if we think about it in three dimensions, with the third dimension abstractly
-      representing progress. This means that circle becomes a spring-like coil.
+      representing progress. This means that the circle becomes a spring-like coil.
     </p>
     <p>
-      With this, we can better visualize this idea in a grander scheme. We can see that although creativity and inspiration continuously cycle in the same two dimensions, the condition of humanity improves almost linearly.
+      With this, we can better visualize this idea in a grander scheme. We can see that, although creativity and inspiration continuously cycle in the same two dimensions, the overall condition of humanity improves almost linearly.
     </p>
     <p>
       I'll be keeping this in the back of my mind for a <em>long</em> time. Hopefully you will too.
