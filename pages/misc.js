8b105169f06ab5cdc02f4b473983e874fa2ffa29
Gerald Nash <me@aunyks.com>
2020-09-08 20:47:54 -0400

    .

--- pages/misc.js
+++ pages/misc.js
@@ -6,6 +6,7 @@ export default () => (
     pageTitle="Misc"
     title="Misc"
     subtitle="Posts of no category">
+    <Link href="/2020/9/hidden-in-plain-sight">hidden in plain sight</Link>
     <Link href="/2020/8/making-the-orb">making the orb</Link>
     <Link href="/2020/8/the-cycle-of-human-progress">the cycle of human progress</Link>
     <Link href="/2020/8/to-be-american-is-to-rebel">to be american is to rebel</Link>
