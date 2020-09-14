aeaf27137a299ed1f756125bb1a50088955ce4bc
Gerald Nash <me@aunyks.com>
2020-09-14 19:42:06 -0400

    .

--- pages/tech/vuln-writeups.js
+++ pages/tech/vuln-writeups.js
@@ -6,7 +6,7 @@ export default () => (
     pageTitle="Vulnerability Writeups"
     title="Vuln Writeups"
     subtitle="Some security vulnerabilities I've found">
-    <Link href="/2020/3/replit-open-redirect">repl.it open url redirection</Link>
     <Link href="/2020/3/substack-open-redirect">substack open url redirection / reflected xss</Link>
+    <Link href="/2020/3/replit-open-redirect">repl.it open url redirection</Link>
   </LinksPage>
 )
