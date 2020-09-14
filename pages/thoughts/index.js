b969b4494f57ceddd04738247b8ee582fa6d115d
Gerald Nash <me@aunyks.com>
2020-09-14 00:34:06 -0400

    .

--- pages/thoughts/index.js
+++ pages/thoughts/index.js
@@ -7,6 +7,6 @@ export default () => (
     subtitle="Some more conclusive than others">
     <Link href="/thoughts/random">random</Link>
     <Link href="/thoughts/on-art">on art</Link>
-    <Link href="/thoughts/on-socialism">on socialism</Link>
+    {/*<Link href="/thoughts/on-socialism">on socialism</Link>*/}
   </LinksPage>
 )
