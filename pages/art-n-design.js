e69f45907cbab5f5cbf22ed5241fdd33bb41989d
Gerald Nash <me@aunyks.com>
2020-10-04 14:31:52 -0400

    Add making serenity's arrival

--- pages/art-n-design.js
+++ pages/art-n-design.js
@@ -5,6 +5,7 @@ export default () => (
   <LinksPage
     pageTitle="Art & Design"
     title="Art & Design">
+    <Link href="/2020/10/making-serenitys-arrival">making serenity's arrival</Link>
     <Link href="/2020/9/how-3d-design-works">how 3d design works</Link>
     <Link href="/2020/9/making-haze">making haze</Link>
     <Link href="/2020/8/making-the-orb">making the orb</Link>
