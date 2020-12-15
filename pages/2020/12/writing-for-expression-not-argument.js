d1a23b39483062eba3b9509568d8bf5aacde0d61
Gerald Nash <me@aunyks.com>
2020-12-15 01:45:58 -0500

    writing for expression not just argument

---
+++ pages/2020/12/writing-for-expression-not-argument.js
@@ -0,0 +1,27 @@
+import Post from 'components/Post'
+
+export default function WritingForExpression() {
+  return (
+    <Post
+      title="Writing For The Sake Of Expression, Not For Argument"
+      description="For too long have I been writing in pure objectivity. Now I'm gonna spend some time writing casual, opinionated articles as well."
+      date="2020-12-14"
+      remark={<></>}>
+      <p>
+        I used to love argumentative writing as I was taught in high school. I loved this so much that I used its techniques in all of my expository writing, like in my <a target="_blank" href="https://medium.com/@aunyks">older articles</a>.
+      </p>
+      <p>
+        I enjoy the research and pursuit of truth that it entails. Arguing for <em>what</em> is right instead of <em>who</em> is right leads us to a shared set of knowledge.
+        It’s an effective way to communicate, as it applies logic that, given enough evidence, is difficult or impossible to effectively dispute. It's in some ways inspired by the
+        scientific method and thus borrows some of its characteristics, like objectivity and verifiability.
+      </p>
+      <p>
+        As I explore writing as a medium for expression, I can’t help but have a fear that someone would strike each sentence I write as a form of opinion or expression as non-factual or in some way incorrect.
+        I've worked my argumentative muscle out for so long and often that I've forgotten how to write <a target="_blank" href="https://www.merriam-webster.com/dictionary/prose">prose</a> and just...well...write!
+      </p>
+      <p>
+        I'm gonna try to mixing my writing styles up so that this blog isn't just a bunch of informative and argumentative essays. I’m teaching myself to write not only for correctness but also for expression: it feels much more natural that way.
+      </p>
+    </Post>
+  )
+}
--- pages/misc.js
+++ pages/misc.js
@@ -7,6 +7,7 @@ export default function Misc() {
       pageTitle="Misc"
       title="Misc"
       subtitle="Posts of no category">
+      <Link href="/2020/12/writing-for-expression-not-argument">writing for the sake of expression, not argument</Link>
       <Link href="/2020/11/roam-the-web-human-hive-mind">roam, the web, and the human hive mind</Link>
       <Link href="/2020/9/hidden-in-plain-sight">hidden in plain sight</Link>
       <Link href="/2020/8/the-cycle-of-human-progress">the cycle of human progress</Link>
