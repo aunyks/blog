feae3af6008528636c5dc13ce86795d6a2561de5
Gerald Nash <me@aunyks.com>
2021-01-06 16:22:10 -0500

    hide just for now

--- pages/misc.js
+++ pages/misc.js
@@ -11,7 +11,7 @@ export default function Misc() {
       <Link href="/2020/11/roam-the-web-human-hive-mind">roam, the web, and the human hive mind</Link>
       <Link href="/2020/9/hidden-in-plain-sight">hidden in plain sight</Link>
       <Link href="/2020/8/the-cycle-of-human-progress">the cycle of human progress</Link>
-      <Link href="/2020/8/to-be-american-is-to-rebel">to be american is to rebel</Link>
+      {/*<Link href="/2020/8/to-be-american-is-to-rebel">to be american is to rebel</Link>*/}
       <Link href="/2020/8/to-be-a-jack-of-all-trades">to be a jack of all trades</Link>
     </LinksPage>
   )
