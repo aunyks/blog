bc8cf5768a4849a79d3b488ded25092bf2851b7f
Gerald Nash <me@aunyks.com>
2021-01-21 00:08:21 -0500

    tidy up and add to web game dev bullets

--- pages/notes/web-game-dev.js
+++ pages/notes/web-game-dev.js
@@ -59,14 +59,25 @@ function updateGameState(dt) {
       <h2>Extra Notes</h2>
       <ul className="mb-4">
         <li>You can use <a target="_blank" href="https://www.pixijs.com">pixi.js</a> (2D), <a target="_blank" href="https://threejs.org">three.js</a> (3D), or the <a target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API">Canvas API</a> (2D) for rendering your game</li>
+        <li><a target="_blank" href="https://rapier.rs/">Rapier.rs</a> as a 3D WASM physics engine</li>
         <li><a target="_blank" href="https://gamedev.stackexchange.com/questions/32631/easy-way-to-do-gravity-in-a-simple-game">Add gravity to your game (engine)</a></li>
         <li><a target="_blank" href="https://gamedev.stackexchange.com/questions/60008/smooth-jumping-in-2d-platformers">Add jump physics to your game (engine)</a></li>
       </ul>
       <h2>Useful Code Snippets</h2>
       <ul className="mb-4">
         <li><a href="/notes/code-snippets/html#basic-game-loop">Basic Game Loop Web Page</a></li>
-        <li><a href="/notes/code-snippets/javascript#gamepad-manager-browser">Gamepad Input Manager</a></li>
-        <li><a href="/notes/code-snippets/javascript#keyboard-manager-game-dev">Keyboard Input Manager</a></li>
+        <li>Input Management & Response
+          <ul>
+            <li><a href="/notes/code-snippets/javascript#gamepad-manager-browser">Gamepad Input Manager</a></li>
+            <li><a href="/notes/code-snippets/javascript#keyboard-manager-game-dev">Keyboard Input Manager</a></li>
+          </ul>
+        </li>
+        <li>
+          Interpolation / Easing Functions
+          <ul>
+            <li><a href="/notes/code-snippets/javascript#linear-interpolation-lerp">Linear (lerp)</a></li>
+          </ul>
+        </li>
       </ul>
     </NotesPost>
   )
