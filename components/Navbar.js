d586ce9ad14e5d4b34f541a3bdde1b7676281793
Gerald Nash <me@aunyks.com>
2021-03-22 03:08:54 -0400

    get started upgrading

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,7 +6,7 @@ import NextLink from 'next/link'
 
 const NavLink = ({ href, children }) => (
   <NextLink href={href}>
-    <a className="text-base no-underline block mt-4 lg:inline-block lg:mt-0 mr-4">
+    <a className="text-sm no-underline block mt-4 lg:inline-block lg:mt-0 mr-4">
       {children}
     </a>
   </NextLink>
@@ -26,15 +26,15 @@ export default function Navbar() {
   return (
     <nav id="navbar"
       style={{ boxShadow: '0px 0px 2px #000' }}
-      className="z-50 bg-white top-0 inset-x-0 fixed flex items-center justify-between flex-wrap px-3 py-3 lg:px-5">
+      className="z-50 bg-white top-0 inset-x-0 fixed flex items-center justify-between flex-wrap px-3 py-3 lg:px-24">
       <div className="flex items-center flex-shrink-0 mr-6">
-        <a href="/" style={{ textColor: 'black' }} className="text-xl font-semibold tracking-tight no-underline">
+        <a href="/" style={{ textColor: 'black' }} className="text-base font-semibold tracking-tight no-underline">
           Hi-Voltage
         </a>
       </div>
       <div className="block lg:hidden">
         <span onClick={() => setNavbarOpen(!isOpen)} className="flex items-center">
-          <svg className=" h-4 w-4" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+          <svg className="h-4 w-4" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
             <title>Menu</title>
             <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
           </svg>
@@ -43,33 +43,29 @@ export default function Navbar() {
       <div style={{ display: `${isOpen ? 'flex' : 'none'}` }} id="navbar-items" className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
         <div className="lg:flex-grow">
           {/* LEFT SIDE OF NAV (LARGE SCREEN) */}
-        </div>
-        <div>
-          {/* RIGHT SIDE OF NAV (LARGE SCREEN) */}
+          <NavLink href="/music">
+            All
+          </NavLink>
+          <NavLink href="/music">
+            Posts
+          </NavLink>
+          <NavLink href="/music">
+            Videos
+          </NavLink>
           <NavLink href="https://aunyks.com/about">
             About
           </NavLink>
-          <NavLink href="/music">
-            Music
-          </NavLink>
-          <NavLink href="/tech">
-            Tech
-          </NavLink>
-          <NavLink href="/art-n-design">
-            Art & Design
-          </NavLink>
-          <NavLink href="/misc">
-            Misc
-          </NavLink>
-          <NavLink href="/tech-culture">
-            Tech&#8596;Culture
-          </NavLink>
-          <NavLink href="/thoughts">
+        </div>
+        <div>
+          {/* RIGHT SIDE OF NAV (LARGE SCREEN) */}
+          {/*
+            <NavLink href="/thoughts">
             Thoughts
           </NavLink>
           <NavLink href="/notes">
             Notes
           </NavLink>
+          */}
         </div>
       </div>
     </nav>
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,42 @@
-import LinksPage from 'components/LinksPage'
-import Link from 'components/Link'
+import Head from 'next/head'
+import Navbar from 'components/Navbar'
 
 export default function HomePage() {
   return (
-    <LinksPage
-      pageTitle="Home"
-      title="Hi-Voltage"
-      subtitle="Increasing potential">
-      <Link href="/music">music</Link>
-      <Link href="/tech">tech</Link>
-      <Link href="/art-n-design">art & design</Link>
-      <Link href="/misc">misc</Link>
-      <Link href="/tech-culture">culture&#8596;tech</Link>
-      <Link href="/thoughts">thoughts</Link>
-      <Link href="/notes">notes</Link>
-      <Link href="https://medium.com/@aunyks">old posts</Link>
-    </LinksPage>
+    <>
+      <Head>
+        <title>Hi-Voltage</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Increasing potential" />
+        <meta name="author" content="Gerald Nash" />
+        <meta name="theme-color" content="#ffffff" />
+        <link rel="icon" type="image/png" href="/favicon.png" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@aunyks" />
+        <meta name="twitter:creator" content="@aunyks" />
+        <meta name="twitter:title" content="Hi-Voltage" />
+        <meta name="twitter:description" content="Increasing potential" />
+        <meta name="twitter:image" content="https://blog.aunyks.com/img/default-card-image.png" />
+      </Head>
+      <Navbar />
+      <style>{`
+        #__next {
+          height: 100vh;
+        }
+      `}</style>
+      <main className="w-full h-full">
+        <section className="flex flex-col justify-center w-full h-full" style={{ background: 'red' }}>
+          <div id="greeting" className="px-3 lg:px-24 mt-20">
+            <h1 className="text-4xl">
+              Hi-Voltage
+          </h1>
+            <h2 className="text-xl">
+              Increasing potential
+          </h2>
+          </div>
+        </section>
+      </main>
+    </>
   )
 }
