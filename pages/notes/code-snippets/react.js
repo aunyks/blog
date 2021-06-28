24701a8fad9e088a3478c371d50f8c2f8e7bc442
Gerald Nash <me@aunyks.com>
2021-06-27 22:00:28 -0400

    Update game template

--- pages/notes/code-snippets/react.js
+++ pages/notes/code-snippets/react.js
@@ -16,33 +16,47 @@ export default function ReactCodeSnippets() {
         </p>
         <CodeBlock lang="jsx">{`
 import {
+  Suspense
+} from 'react'
+import dynamic from 'next/dynamic'
+import {
   Canvas
 } from '@react-three/fiber'
 import {
   Physics
 } from '@react-three/cannon'
 import FlatGround from 'components/3d/FlatGround'
-import FirstPersonPlayer from 'components/3d/FirstPersonPlayer'
-import GameDirector from 'components/3d/GameDirector'
+import Terrain from 'components/3d/Terrain'
 import SkyDome from 'components/3d/SkyDome'
+import Sun from 'components/3d/Sun'
+import GameDirector from 'components/3d/GameDirector'
+import PauseManager from 'components/3d/PauseManager'
+import FirstPersonPlayer from 'components/3d/FirstPersonPlayer'
+
+function GameLoading() {
+  return (
+    <p>Loading...</p>
+  )
+}
 
 export default function Game() {
   return (
     <div style={{ height: '100vh', width: '100vw' }}>
-      <Canvas>
-        <Physics>
-          <GameDirector defaultCam="First Person Cam">
-            <mesh position={[0, 0, -2]}>
-              <boxBufferGeometry />
-              <meshBasicMaterial color={0x0000ff} />
-            </mesh>
-            <FirstPersonPlayer />
+      <Canvas frameloop="demand">
+        <Suspense fallback={<GameLoading />}>
+          <Physics shouldInvalidate={false}>
+            <PauseManager>
+              <GameDirector defaultCam="First Person Cam">
+                <FirstPersonPlayer startPosition={[0, 10, 0]} />
+              </GameDirector>
+            </PauseManager>
+            <Sun position={[0, 1000, -1000]} />
             <SkyDome />
-            <FlatGround />
-          </GameDirector>
-        </Physics>
+            <Terrain />
+          </Physics>
+        </Suspense>
       </Canvas>
-    </div>
+    </div >
   )
 }
 `}</CodeBlock>
