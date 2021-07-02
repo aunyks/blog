3582807e7fbe08d46803376306f479e3d0d7e68a
Gerald Nash <me@aunyks.com>
2021-07-01 21:51:45 -0400

    Add 3D Figure component for using 3D scenes in posts

---
+++ components/3d/ThreeDFigure.js
@@ -0,0 +1,69 @@
+import {
+  useState,
+  useEffect
+} from 'react'
+import {
+  Canvas, useFrame
+} from '@react-three/fiber'
+
+function FrameManager({
+  isPaused,
+  children
+}) {
+  useFrame(({ invalidate }) => {
+    if (!isPaused) {
+      invalidate()
+    }
+  })
+  return (
+    <>
+      {children}
+    </>
+  )
+}
+
+export default function ThreeDFigure({
+  caption,
+  children,
+  ...props
+}) {
+  const [isPaused, setPaused] = useState(false)
+
+  return (
+    <>
+      <style jsx>{`
+      figure {
+        position: relative;
+        height: 50vh;
+        margin-bottom: ${!!caption ? '2em' : '1em'};
+      }
+
+      button {
+        position: absolute;
+        background: rgba(127, 127, 127, 0.5);
+        bottom: 5px;
+        left: 5px;
+        z-index: 1;
+      }
+    `}</style>
+      <figure>
+        <Canvas
+          frameloop="demand"
+          gl={{
+            powerPreference: 'low-power'
+          }}
+          {...props}>
+          <FrameManager isPaused={isPaused}>
+            {children}
+          </FrameManager>
+        </Canvas>
+        {/* <button aria-label="Toggle playing / pausing this figure" onClick={() => {
+          setPaused(!isPaused)
+        }}>
+          {isPaused ? 'Play' : 'Pause'}
+        </button> */}
+        {caption}
+      </figure>
+    </>
+  )
+}
