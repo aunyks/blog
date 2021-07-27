a4be27c2f04a407467c8392799face14887d9d3e
Gerald Nash <me@aunyks.com>
2021-07-27 00:05:19 -0400

    Document tha top level folders of the project and add swr fetchers for web projects to utils, also create utils lol

--- README.md
+++ README.md
@@ -3,4 +3,17 @@ Where I use my writings, notes, and thoughts in a variety of forms and categorie
 the most important projects on my domain.
   
 
-Also hosted at hivoltage.xyz.
+Also hosted at hivoltage.xyz.  
+
+This is a Next.js project
+
+## Folders
+
+- `components` - React components used by pages and other components
+- `contexts` - React contexts used by pages and, optionally, components
+- `global-fonts` - Font files to be imported into prebuilt CSS (see `styles/index.css` font imports and `tailwind.config.js` `fontFamily` value)
+- `hooks` - React hooks used by pages and components
+- `pages` - Next.js pages
+- `public` - Public, static files to be used by components, pages, or external domains
+- `styles` - CSS file(s) that are built with Tailwind and made available to all of the project's pages
+- `utils` - Code that can be reused but can't be accurately described by the other folders
---
+++ utils/swr-fetchers/http-json.js
@@ -0,0 +1,36 @@
+import useSWR from 'swr'
+
+// This assumes that the api will return json 
+// *even if the status code isn't 2xx*
+const httpJsonFetcher = async url => {
+  const res = await fetch(url)
+  // If the status code is not in the range 200-299,
+  // we still try to parse and throw it
+  if (!res.ok) {
+    const error = new Error('An error occurred while fetching the data.')
+    // Attach extra info to the error object.
+    error.status = res.status
+    error.info = await res.json()
+    throw error
+  }
+  return res.json()
+}
+
+// Example user / auth hook. Can call sign in and out separately 
+// since on they'll handle cookies. This will just work, as long as those handle 
+// receiving & deleting cookies
+// function useUser (id) {
+//   const { data, error } = useSWR(`/api/user/${id}`, httpJsonFetcher)
+//   return {
+//     user: data,
+//     isLoading: !error && !data,
+//     error
+//   }
+// }
+
+
+//   if (error) return <div>failed to load</div>
+//   if (!data) return <div>loading...</div>
+//   return <div>hello {data.name}!</div>
+
+export default httpJsonFetcher
