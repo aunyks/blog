ec868fe168b83d0df05bc46855f51de30b975e4f
Gerald Nash <me@aunyks.com>
2021-08-07 21:48:07 -0400

    giving you a taste of what's to come. smart of you to look for / at this

---
+++ hooks/3d/use-animations.js
@@ -0,0 +1,55 @@
+import {
+  useRef,
+  useState,
+  useEffect
+} from 'react'
+import {
+  useFrame
+} from '@react-three/fiber'
+import {
+  Object3D,
+  AnimationMixer
+} from 'three'
+
+// From 
+// https://github.com/pmndrs/drei/blob/ac5f687f2b6d38c3aaac4c6d700987e95adfc14c/src/core/useAnimations.tsx
+export default function useAnimations(
+  clips,
+  root
+) {
+  const ref = useRef()
+  const [actualRef] = useState(() => (root ? (root instanceof Object3D ? { current: root } : root) : ref))
+  const [mixer] = useState(() => new AnimationMixer(null))
+  const lazyActions = useRef({})
+  const [api] = useState(() => {
+    const actions = {}
+    clips.forEach((clip) =>
+      Object.defineProperty(actions, clip.name, {
+        enumerable: true,
+        get() {
+          if (actualRef.current) {
+            return (
+              lazyActions.current[clip.name] ||
+              (lazyActions.current[clip.name] = mixer.clipAction(clip, actualRef.current))
+            )
+          }
+        },
+      })
+    )
+    return { ref: actualRef, clips, actions, names: clips.map((c) => c.name), mixer }
+  })
+  useFrame((state, delta) => mixer.update(delta))
+  useEffect(() => {
+    const currentRoot = actualRef.current
+    return () => {
+      // Clean up only when clips change, wipe out lazy actions and uncache clips
+      lazyActions.current = {}
+      Object.values(api.actions).forEach((action) => {
+        if (currentRoot) {
+          mixer.uncacheAction(action, currentRoot)
+        }
+      })
+    }
+  }, [clips])
+  return api
+}
