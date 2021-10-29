0abb1d599e5347aa23f5a51d3a42cd8ad290e564
Nash <me@aunyks.com>
2021-10-29 15:02:36 -0400

    Better copyright dating

--- components/PostFooter.js
+++ components/PostFooter.js
@@ -1,28 +1,29 @@
-export default function PostFooter({
-  date,
-  lang,
-  textDirection,
-  remark
-}) {
+export default function PostFooter({ date, lang, textDirection, remark }) {
   let year = null
   if (date) {
     const d = new Date(date)
     year = new Intl.DateTimeFormat(lang || 'en', { year: 'numeric' }).format(d)
   }
   return (
-    <footer id="footer" lang={lang || 'en'} style={{ textDirection: (textDirection || 'ltr') }}>
+    <footer
+      id="footer"
+      lang={lang || 'en'}
+      style={{ textDirection: textDirection || 'ltr' }}>
       <hr className="mt-3" />
       <p>
         {remark || (
           <>
-            Thanks for reading. Feel free to follow me on <a href="https://twitter.com/intent/follow?screen_name=aunyks">Twitter</a> if you have any
-          questions about this post or just wanna chat.
+            Thanks for reading. Feel free to follow me on{' '}
+            <a href="https://twitter.com/intent/follow?screen_name=aunyks">
+              Twitter
+            </a>{' '}
+            if you have any questions about this post or just wanna chat.
           </>
         )}
       </p>
       <p className="select-none">
-        Copyright &copy; {date && `2019-${year}`} Gerald Nash
-        </p>
+        Copyright &copy; {date && `${year}-2021`} Gerald Nash
+      </p>
     </footer>
   )
 }
