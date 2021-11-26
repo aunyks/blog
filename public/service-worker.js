29afb2d0c97f4a20d10cf9c6a5281d3ce102c192
Nash <me@aunyks.com>
2021-11-26 00:06:36 -0500

    Fix certain media not showing up on initial page load (edit service worker)

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,23 +1,31 @@
 // Name of our cache for reference in-browser
 const cacheName = 'hivoltage-cache'
 // Assets to download to cache on install
-const staticAssets = [
-  '/',
-]
+const staticAssets = ['/']
 
-self.addEventListener('install', async e => {
+self.addEventListener('install', async (e) => {
   const cache = await caches.open(cacheName)
   await cache.addAll(staticAssets)
   return self.skipWaiting()
 })
 
-self.addEventListener('activate', e => {
+self.addEventListener('activate', (e) => {
   self.clients.claim()
 })
 
-self.addEventListener('fetch', async e => {
+self.addEventListener('fetch', async (e) => {
   const req = e.request
   const url = new URL(req.url)
+  // Ignore certain media types and let their
+  // requests pass through
+  if (
+    url.pathname.endsWith('.mp4') ||
+    url.pathname.endsWith('.mp3') ||
+    url.pathname.endsWith('.wav') ||
+    url.pathname.endsWith('.ogg')
+  ) {
+    return
+  }
   e.respondWith(networkAndCache(req))
 })
 
@@ -29,16 +37,18 @@ async function cacheFirst(req) {
   return cached || fetch(req)
 }
 
-// Use this to respond to requests from the network 
+// Use this to respond to requests from the network
 // first and fall back to cache if not available
 async function networkAndCache(req) {
   const cache = await caches.open(cacheName)
   try {
     const fresh = await fetch(req)
-    // If we're fetching something useful (not a Chrome extension), 
+    // If we're fetching something useful (not a Chrome extension),
     // let's cache it
     const urlProtocol = new URL(req.url).protocol
-    if (req.method === 'GET' && urlProtocol.startsWith('http')) {
+    const shouldCacheResponse =
+      req.method === 'GET' && urlProtocol.startsWith('http')
+    if (shouldCacheResponse) {
       await cache.put(req, fresh.clone())
     }
     return fresh
