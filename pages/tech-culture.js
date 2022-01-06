4c69790ddc75b464341b9b38fe17afce68e864a4
Nash <me@aunyks.com>
2022-01-05 21:47:44 -0500

    Add prettier and add web braclets to tech & culture category

--- pages/tech-culture.js
+++ pages/tech-culture.js
@@ -7,10 +7,19 @@ export default function TechCulture() {
       pageTitle="Tech & Culture"
       title="Tech&#8596;Culture"
       subtitle="Technology is culture">
-      <Link href="/2021/6/in-search-of-the-gamedao">in search of the GameDAO</Link>
-      <Link href="/2020/9/future-of-tech-for-we">the future of tech for we</Link>
-      <Link href="/2020/7/3d-modeling-meets-fashion-consumers">3D modeling meets fashion consumers</Link>
-      <Link href="/2020/4/techwear-cyberpunk-in-contemporary-fashion">techwear: cyberpunk in contemporary fashion</Link>
+      <Link href="/2022/1/web-bracelets">web bracelets</Link>
+      <Link href="/2021/6/in-search-of-the-gamedao">
+        in search of the GameDAO
+      </Link>
+      <Link href="/2020/9/future-of-tech-for-we">
+        the future of tech for we
+      </Link>
+      <Link href="/2020/7/3d-modeling-meets-fashion-consumers">
+        3D modeling meets fashion consumers
+      </Link>
+      <Link href="/2020/4/techwear-cyberpunk-in-contemporary-fashion">
+        techwear: cyberpunk in contemporary fashion
+      </Link>
       <Link href="/2020/4/masks-will-be-the-norm">masks will be the norm</Link>
     </LinksPage>
   )
